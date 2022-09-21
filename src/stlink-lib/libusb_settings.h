e28e8f1430ffd42c3c879397a09b990cdcedaa89
RVeips <rihardsveips@gmail.com>
2022-09-21 22:55:07 +0300

    more build fixes...

--- src/stlink-lib/libusb_settings.h
+++ src/stlink-lib/libusb_settings.h
@@ -1,6 +1,7 @@
 #ifndef LIBUSB_SETTINGS_H
 #define LIBUSB_SETTINGS_H
 
+#define _SSIZE_T_DEFINED 1
 #include <libusb.h>
 
 /*
@@ -21,28 +22,28 @@
  *  v1.0.25    | 0x01000109
  */
 
-#if defined (__FreeBSD__)
-    #if !defined (LIBUSBX_API_VERSION)
-        #define LIBUSBX_API_VERSION LIBUSB_API_VERSION
-    #elif !defined (LIBUSB_API_VERSION)
-        #error unsupported libusb version
-    #endif
+#if defined(__FreeBSD__)
+#if !defined(LIBUSBX_API_VERSION)
+#define LIBUSBX_API_VERSION LIBUSB_API_VERSION
+#elif !defined(LIBUSB_API_VERSION)
+#error unsupported libusb version
+#endif
 #endif
 
-#if defined (__FreeBSD__)
-    #define MINIMAL_API_VERSION 0x01000102 // v1.0.16
-#elif defined (__OpenBSD__)
-    #define MINIMAL_API_VERSION 0x01000105 // v1.0.21
-#elif defined (__linux__)
-    #define MINIMAL_API_VERSION 0x01000105 // v1.0.21
-#elif defined (__APPLE__)
-    #define MINIMAL_API_VERSION 0x01000109 // v1.0.25
-#elif defined (_WIN32)
-    #define MINIMAL_API_VERSION 0x01000109 // v1.0.25
+#if defined(__FreeBSD__)
+#define MINIMAL_API_VERSION 0x01000102 // v1.0.16
+#elif defined(__OpenBSD__)
+#define MINIMAL_API_VERSION 0x01000105 // v1.0.21
+#elif defined(__linux__)
+#define MINIMAL_API_VERSION 0x01000105 // v1.0.21
+#elif defined(__APPLE__)
+#define MINIMAL_API_VERSION 0x01000109 // v1.0.25
+#elif defined(_WIN32)
+#define MINIMAL_API_VERSION 0x01000109 // v1.0.25
 #endif
 
 #if (LIBUSB_API_VERSION < MINIMAL_API_VERSION)
-    #error unsupported libusb version
+#error unsupported libusb version
 #endif
 
 #endif // LIBUSB_SETTINGS_H
